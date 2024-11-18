5c65e4f4fb0a260ff516eddf38189e0d6a45cb9b
Andrey-Peregudov <anmrya12@gmail.com>
2024-11-18 21:32:47 +0300

    weather info

--- app/weather.py
+++ app/weather.py
@@ -1,5 +1,13 @@
-import request
+import requests
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+api_key = os.getenv('API_KEY')
 
 def get_lan_lon(city_name, state_code, country_code, API_key):
-    resp = request.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_key}')
+    resp = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code},{country_code}&appid={API_key}').json()
+    print(resp)
+get_lan_lon('Toronto', 'ON', "Canada", api_key)
+
+
